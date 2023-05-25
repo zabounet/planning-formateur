1025453f59a1915f464f6121112c49a44b280872
Ali Akbari <aakkbari@campus-centre.fr>
2023-05-25 10:33:48 +0200

    dd

--- Views/main/index.php
+++ Views/main/index.php
@@ -1,3 +1,15 @@
 <?php $link = "Home" ;?>
-
+<div class="legend">
+    <ul>
+        <li value="<?= $_SESSION['color']['couleur_centre']; ?>">1</li>
+        <li value="<?= $_SESSION['color']['couleur_pae']; ?>">2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>9</li>
+        <li>10</li>
+    </ul>
+</div>
 <?= $html;?>
